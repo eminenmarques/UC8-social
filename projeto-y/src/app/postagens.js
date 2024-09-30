31eea47324356b085974aa3442afb9bb71dd850f
Eminen <eminemarque@gmail.com>
2024-09-30 16:16:40 -0300

    vv

--- projeto-y/src/app/postagens.js
+++ projeto-y/src/app/postagens.js
@@ -3,7 +3,7 @@ import styles from "./postagens.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.css}>
+    <div className={styles.container}>
       <div className={styles.perfil}></div>
       <div className={styles.Mensagem}></div>
       <div className={styles.intaracao}></div>
