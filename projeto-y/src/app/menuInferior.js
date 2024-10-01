d44001daca89d44775b32e99e155a8c78955b22d
Pedro <pedroarmiliato2006@gmail.com>
2024-10-01 16:55:08 -0300

    Add Footer

--- projeto-y/src/app/menuInferior.js
+++ projeto-y/src/app/menuInferior.js
@@ -1,5 +1,23 @@
 import styles from "./menuInferior.module.css";
 
 export default function MenuInferior() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.mais}>
+        <img src="/burgermenu.svg"></img>
+      </button>
+      <button className={styles.trends}>
+        <img src="/planet.svg"></img>
+      </button>
+      <button className={styles.digitar}>
+        <img src="/notepaddark.svg"></img>
+      </button>
+      <button className={styles.batepapo}>
+        <img src="/speechdark.svg"></img>
+      </button>
+      <button className={styles.conta}>
+        <img src="/userdark.svg"></img>
+      </button>
+    </div>
+  );
 }
